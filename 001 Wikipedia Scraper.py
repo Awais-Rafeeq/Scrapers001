d87cbd1990310f8fb010a9738fea9fd22f89e573
Awais Rafeeq <awaisrafeeq692@gmail.com>
2024-04-15 22:09:34 +0500

    updated file

--- 001 Wikipedia Scraper.py
+++ 001 Wikipedia Scraper.py
@@ -25,3 +25,5 @@ def fetch_and_save_to_file(url, path):
 
 # Calling the function with the specified URL and file path
 fetch_and_save_to_file(url, "data/wikipedia_data_analysis.html")
+
+#a new line added
